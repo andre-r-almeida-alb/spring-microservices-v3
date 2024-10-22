685eb66647947a88fdc40f19cfe60e7c7defa794
André Rodrigues Almeida <andre-r-almeida@alticelabs.com>
2024-10-22 16:40:31 +0100

    My changes in limits service

--- 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ 03.microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -1,22 +1,21 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.Limits;
 import com.in28minutes.microservices.limitsservice.configuration.Configuration;
 
+@RequiredArgsConstructor
 @RestController
 public class LimitsController {
 
-	@Autowired
-	private Configuration configuration;
+	private final Configuration configuration;
 
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
 		return new Limits(configuration.getMinimum(), 
 				configuration.getMaximum());
-//		return new Limits(1,1000);
 	}
 }
